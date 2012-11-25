e557fbd5150e6762fde5847660752edb3e8f7232
Matt Blair <duereg@hotmail.com>
2012-11-25 20:18:51 +0800

    #1 Fixed issue where path was not correct in relation to the package repo path.

--- JsValidate.py
+++ JsValidate.py
@@ -3,6 +3,6 @@ import sublime, sublime_plugin
 class JsvalidateCommand(sublime_plugin.WindowCommand):
   def run(self):
     self.window.run_command('set_build_system', {
-      'file': 'Packages/JsValidate/JsValidate.sublime-build'
+      'file': 'Packages/JS Validation using Esprima/JsValidate.sublime-build'
     })
     self.window.run_command('build')
